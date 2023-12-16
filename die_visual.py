6ddd4e77f552d337f3e117b5f53b2ecd101e49b6
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2023-12-16 18:38:05 +0600

    Анализ данных по 6 граням кубика

--- die_visual.py
+++ die_visual.py
@@ -1,11 +1,16 @@
 from die import Die
 
-die = Die() # создаем кубик
+# создаем кубик
+die = Die()
 results = []
 for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
+# анализ результатов
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
 
-
-print(results)
+print(frequencies)
